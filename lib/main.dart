73172d426599499b1242fe22e0014f1677450c10
hernan74 <hernanlopez74@gmail.com>
2021-03-03 18:40:57 -0300

    se agrego dateField

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/routes/route_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
  
@@ -8,10 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,localizationsDelegates: [
+         GlobalMaterialLocalizations.delegate,
+         GlobalWidgetsLocalizations.delegate,
+         GlobalCupertinoLocalizations.delegate,
+      ],supportedLocales: [
+         const Locale('en', ''), // English, 
+        const Locale('es', 'ES'), // Español, 
+    
+      ],
       title: 'Material App',
       initialRoute: '/',
       routes:getRoutePages(),
-      );
+   );
   }
 }
